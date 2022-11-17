232d72ef24228d3699bee8f9492f336b6e167c17
“Filip <filip@bratia.sk>
2022-11-17 20:26:37 +0100

    Fix path

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -2,15 +2,13 @@
 
 namespace App\Controller;
 
-use App\Entity\Measurement;
-use App\Repository\MeasurementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AboutController extends AbstractController
 {
-    #[Route(path: '/o-projekte', name: 'about')]
+    #[Route(path: 'o-projekte', name: 'about')]
     function index(): Response
     {
         return $this->render('About/index.html.twig');
